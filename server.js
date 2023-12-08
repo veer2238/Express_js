3fd171c8d5be55383d0dfc9dc46a92a29bfa8567
veer2238 <veer2238rajput@gmail.com>
2023-12-08 06:58:47 +0530

    server-config

--- server.js
+++ server.js
@@ -1,51 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require('express')
+const app = express()
 
-const server = express();
 
-server.use(cors());
-server.use(bodyParser.json());
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
 
-server.post('/', async (req, res) => {
-  try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: 'veer2238rajput@gmail.com',
-        pass: 'jeasqtbfommzmxya'
-      },
-    });
+app.listen(3000, () => {
+console.log("server connected")
+})
 
-    let info = await transporter.sendMail({
-      from: 'veer2238rajput@gmail.com',
-      to: 'lalu2238rajput@gmail.com',
-      subject: "thanks for registration",
-      text: "Hello world?",
-      html: req.body.name + "<br>" + req.body.phone + "<br>" + req.body.message
-    });
-
-    let info1 = await transporter.sendMail({
-      from: 'veer2238rajput@gmail.com',
-      to: req.body.email,
-      subject: "thanks for registration",
-      text: "Hello world?",
-      html: "thanks for registration"
-    });
-
-    console.log("Message sent: %s", info.messageId, info1.messageId);
-
-    // Respond with success
-    res.status(200).json({ success: true, message: 'Emails sent successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ success: false, error: 'Internal server error' });
-  }
-});
-
-server.listen(8080, () => {
-  console.log("Server connected");
-});
+//Run the app with the following command
+//node app.js
