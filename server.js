492abf70833c74d4db07d6b95c3e5721784f56ec
veer2238 <veer2238rajput@gmail.com>
2023-12-09 18:15:08 +0530

    server-config

--- server.js
+++ server.js
@@ -1,20 +1,82 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
+const server = express();
 
-app.get('/', (req, res) => {
+server.use(cors());
+server.use(bodyParser.json());
 
+server.post('/contact', async (req, res) => {
   try {
-    res.send('Hello World!')
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'veer2238rajput@gmail.com',
+        pass: 'jeasqtbfommzmxya'
+      },
+    });
+
+  
+
+    let info = await transporter.sendMail({
+      from: 'veer2238rajput@gmail.com',
+      to: req.body.email,
+      subject: "thanks for registration",
+      text: "Hello world?",
+      html: req.body.name + "<br>" + req.body.phone + "<br>" + req.body.message
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    // Respond with success
+    res.status(200).json({ success: true, message: 'Emails sent successfully' });
   } catch (error) {
-    res.status("ok")
+    console.error('Error:', error);
+    res.status(500).json({ success: false, error: 'Internal server error' });
   }
+});
 
+
+server.post('/attendance', async (req, res) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'veer2238rajput@gmail.com',
+        pass: 'jeasqtbfommzmxya'
+      },
+    });
+
+  
+
+    let info = await transporter.sendMail({
+      from: 'veer2238rajput@gmail.com',
+      to: 'himanshu0409agraval@gmail.com',
+      subject: "thanks for registration",
+      text: "Hello world?",
+      html: "my name is " +req.body.name + "<br>" +"i attend "+ req.body.date +" lecture"+ "<br>" +"my work:"+ req.body.work
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    // Respond with success
+    res.status(200).json({ success: true, message: 'Emails sent successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false, error: 'Internal server error' });
+  }
+});
+
+server.get('/',(req,res) =>{
+  res.send("welcome")
 })
 
-app.listen(3000, () => {
-console.log("server connected")
-})
-
-//Run the app with the following command
-//node app.js
+server.listen(8080, () => {
+  console.log("Server connected");
+});
